ecece05e3b0e978e33d2c8e1c9a6b574b0651666
Joel Svensson <svenssonjoel@yahoo.se>
2020-06-17 17:37:18 +0200

    a testnetwork

--- src/Coppe.hs
+++ src/Coppe.hs
@@ -1,11 +1,10 @@
 
 -- Experimentation
 module Coppe
-  (rep
-  ,testweb)
   where 
 
 import Control.Monad.Writer
+import Control.Monad.Trans.State
 
 import Hyperparameters
 
@@ -17,18 +16,73 @@ type Name = String
 data Recipe =
   Input
   | Empty
-  | NamedIntermediate Name
+  | NamedIntermediate Identifier
   | Operation LayerOperation Hyperparameters
   | Seq Recipe Recipe
   deriving (Eq, Show)
 
-
-
-rep :: Int -> Recipe -> Recipe
-rep 0 r = Empty
-rep n r = r `Seq` (rep (n-1) r)
-
-
-testweb :: Recipe
-testweb = Empty
+instance Semigroup Recipe where
+  (<>) = Seq
   
+instance Monoid Recipe where
+  mempty = Empty
+  mappend Empty a = a
+  mappend a Empty = a
+  mappend a b     = Seq a b
+
+type Coppe a = StateT Integer (Writer Recipe) a
+
+name :: Coppe Identifier
+name =
+  do i <- get
+     put (i + 1)
+     tell $ NamedIntermediate (Identifier i)
+     return $ Identifier i
+     
+empty :: Coppe ()
+empty = tell Empty
+
+input :: Coppe ()
+input = tell Input
+
+operation :: LayerOperation -> Hyperparameters -> Coppe ()
+operation op h = tell $ Operation op h
+
+conv :: Hyperparameters -> Coppe ()
+conv h = operation Conv h
+
+batchNormalize :: Hyperparameters -> Coppe ()
+batchNormalize h = operation BatchNormalize h
+
+relu :: Coppe ()
+relu = operation Relu emptyHyperparameters
+
+add :: Hyperparameters -> Coppe ()
+add h = operation Add h
+
+test :: Coppe a -> Coppe (a, Recipe)
+test = listen
+
+rep :: Coppe () -> Integer -> Coppe ()
+rep m 0 = empty
+rep m n = m >> rep m (n-1)
+
+build :: Coppe a -> Recipe
+build m = execWriter $ evalStateT m 0
+
+testNetwork =
+  let convParams = emptyHyperparameters {strides = Just (Strides [1,1])
+                                           ,filters = Just (Filters 16)}
+      addParams = emptyHyperparameters
+  in
+  do input_data <- name 
+     conv convParams  
+     batchNormalize emptyHyperparameters
+     relu
+     conv convParams
+     batchNormalize emptyHyperparameters
+     bn_out <- name
+     add (addParams {inputLayer = Just [input_data, bn_out]})
+
+        
+
