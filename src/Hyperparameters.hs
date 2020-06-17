627df3adbfe9f1ee6337c0acebc0c902fee54d63
Joel Svensson <svenssonjoel@yahoo.se>
2020-06-17 12:40:35 +0200

    added more kinds of hyperparameters

--- src/Hyperparameters.hs
+++ src/Hyperparameters.hs
@@ -5,11 +5,25 @@ import Data.Maybe
 
 data Strides = Strides [Int]
   deriving (Eq, Show)
+
 data Filters = Filters Int
   deriving (Eq, Show)
 
+data Padding = Same
+  deriving (Eq, Show)
+
+data Initialization = Random
+  deriving (Eq, Show)
+
+data Dimensions = Dimensions [Int]
+  deriving (Eq, Show)
+
 data Hyperparameters =
-  Hyperparameters { strides :: Maybe Strides
-                  , filters :: Maybe Filters }
-                  
+  Hyperparameters { strides    :: Maybe Strides
+                  , filters    :: Maybe Filters
+                  , variance   :: Maybe Float
+                  , padding    :: Maybe Padding
+                  , init       :: Maybe Initialization
+                  , kernelSize :: Maybe Dimensions }
+  
   deriving (Eq, Show)
