3bf1b06bba87c1a8372677d2386aa86d6603a13c
Joel Svensson <svenssonjoel@yahoo.se>
2020-06-20 14:40:46 +0200

    Experimenting with representation of Tensor that can hold dimensionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Coppe
+import CoppeB
 
 main :: IO ()
 main = putStrLn "Work in progress"
---
+++ src/Tensor.hs
@@ -0,0 +1,5 @@
+module Tensor where
+
+
+
+data Tensor a = Tensor Integer [Integer] 
