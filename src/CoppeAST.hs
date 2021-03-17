2f9b087d3dfc8609a773cdf9c1d3662bc21020d8
Joel Svensson <svenssonjoel@yahoo.se>
2021-03-17 18:32:21 +0100

    experimenting, currently broken

--- src/CoppeAST.hs
+++ src/CoppeAST.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GADTs #-}
+
 module CoppeAST (
                 -- Layeroperations
                   Ingredient(..)
@@ -13,7 +15,7 @@ module CoppeAST (
                 , Dimensions(..)
                 , Identifier
                 , Hyperparameters(..)
-                , emptyHyperparameters
+             --   , emptyHyperparameters
 
                 -- tensors
                 , Type(..)
@@ -26,6 +28,7 @@ module CoppeAST (
                 ) where
 
 import Data.Maybe
+import qualified Data.Map as Map
 
 -- ------------------------------------------------------------ --
 -- Tensors
@@ -86,45 +89,74 @@ data Dimensions = Dimensions [Integer]
 type Identifier = String
 
 
-data Hyperparameters =
-  Hyperparameters { strides         :: Maybe Strides
-                  , filters         :: Maybe Filters
-                  , variance        :: Maybe Float
-                  , padding         :: Maybe Padding
-                  , initialization  :: Maybe Initialization
-                  , kernelSize      :: Maybe Dimensions
-                  , inputLayer      :: Maybe [Identifier]
-                  , name            :: Maybe Identifier}
-  deriving (Eq, Show)
+-- data Hyperparameters =
+--   Hyperparameters { strides         :: Maybe Strides
+--                   , filters         :: Maybe Filters
+--                   , variance        :: Maybe Float
+--                   , padding         :: Maybe Padding
+--                   , initialization  :: Maybe Initialization
+--                   , kernelSize      :: Maybe Dimensions
+--                   , inputLayer      :: Maybe [Identifier]
+--                   , name            :: Maybe Identifier}
+--   deriving (Eq, Show)
 
 
 
-emptyHyperparameters = Hyperparameters { strides         = Nothing
-                                       , filters         = Nothing
-                                       , variance        = Nothing
-                                       , padding         = Nothing
-                                       , initialization  = Nothing
-                                       , kernelSize      = Nothing
-                                       , inputLayer      = Nothing
-                                       , name            = Nothing}
+-- emptyHyperparameters = Hyperparameters { strides         = Nothing
+--                                        , filters         = Nothing
+--                                        , variance        = Nothing
+--                                        , padding         = Nothing
+--                                        , initialization  = Nothing
+--                                        , kernelSize      = Nothing
+--                                        , inputLayer      = Nothing
+--                                        , name            = Nothing}
+
+
+data Value = IntVal Integer | FloatVal Double
+  | ListVal [ Value ]
+
+type Param = Value
+type Annot = Value
+
+type Hyperparameters = Map.Map String Param
+type Annotation      = Map.Map String Annot
+    
+class Ingredient a  where 
+  name :: a -> String
+  annotation :: a -> Annotation 
+  annotate   :: String -> Value -> a -> a 
+
+
+
+
+
+-- ------------------------------------------------------------ --
+-- Some example ingredients
+
+data Conv = Conv Annotation
+
+instance Ingredient Conv where
+  name _ = "conv"
+  annotation (Conv a) = a
+  annotate s v (Conv a) = Conv $ Map.insert s v a
+
 
 
 -- ------------------------------------------------------------ --
 -- Layers 
 
-
 -- May want annotations on Ingredients
-data Ingredient = Relu
-                | Conv
-                | BatchNormalize 
-                | Add             
-                | Reshape        
-                | Dense          -- Dense feed-forward (Fully connected layer)
-                | UpSampling     -- May be trained, interpolation, 
-                | DownSampling   -- Pooling (function, for example average) Not trained
-                | Padd           -- Add Padding
-                | Concat         -- Along the channel dimension
-  deriving (Eq, Show)
+-- data Ingredient = Relu
+--                 | Conv
+--                 | BatchNormalize 
+--                 | Add             
+--                 | Reshape        
+--                 | Dense          -- Dense feed-forward (Fully connected layer)
+--                 | UpSampling     -- May be trained, interpolation, 
+--                 | DownSampling   -- Pooling (function, for example average) Not trained
+--                 | Padd           -- Add Padding
+--                 | Concat         -- Along the channel dimension
+--   deriving (Eq, Show)
 
 type Name = String
   
@@ -136,31 +168,28 @@ data RecipeAnnotation =
   deriving (Eq, Show)
 
 
-data Recipe =
-  Input 
-  | Empty
-  | Operation Ingredient Hyperparameters
-  | Seq Recipe Recipe
-  | Rep Integer Recipe -- What will the identifiers mean in here?
-  -- Annotations added by traversals  
-  | Annotated RecipeAnnotation Recipe
-  deriving (Eq, Show)
+data Recipe where
+  Input :: Recipe
+  Empty :: Recipe
+  Operation :: (Ingredient a) => a -> Maybe Hyperparameters -> Recipe
+  Seq :: Recipe -> Recipe -> Recipe
+  Annotated :: Annotation -> Recipe
 
 
-data RecipeT a where
-  InputT :: a -> RecipeT a
-  EmptyT :: RecipeT ()
-  OpT    :: Operation a b -> RecipeT (a -> b)
-  AppT   :: RecipeT (a -> b) -> RecipeT a -> RecipeT b 
-  SeqT   :: RecipeT (a -> b) -> RecipeT b
-  Annot  :: RecipeAnnotation -> RecipeT a -> RecipeT a
+-- data Recipe =
+--   Input 
+--   | Empty
+--   | Operation Ingredient Hyperparameters
+--   | Seq Recipe Recipe
+--   | Rep Integer Recipe -- What will the identifiers mean in here?
+--   -- Annotations added by traversals  
+--   | Annotated RecipeAnnotation Recipe
+--   deriving (Eq, Show)
 
 -- Seq (Seq x y) z)
 
 -- Can annotate one layer or a sequence of many layers. 
 -- Seq (Annotated r (Seq x y)  (Seq z (Annotated r1 k) ) 
-
-
 
 instance Semigroup Recipe where
   (<>) = Seq
