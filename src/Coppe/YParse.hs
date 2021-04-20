a28945e80cbdfc7de17f9055986f8a52c5a7740a
Joel Svensson <svenssonjoel@yahoo.se>
2021-04-20 09:28:25 +0200

    messing with YAML. and frustrated with Haskell

--- src/Coppe/YParse.hs
+++ src/Coppe/YParse.hs
@@ -1,10 +1,48 @@
-
-module Coppe.YParse (
-
-       ) where
+{-# LANGUAGE OverloadedStrings #-}
+module Coppe.YParse  where
 
 
 import Data.YAML
+import Data.Text
+import qualified Data.ByteString as BS
+import Data.ByteString.Lazy.UTF8 as BLU
+import qualified Data.Map        as Map
 
-      
+import Prelude as P
+       
+import Coppe.AST
 
+data Label = Label Text
+data Person = Person Text Int
+  deriving Show
+
+instance ToYAML Person where
+    -- this generates a Node
+    toYAML (Person n a) = mapping [ "name" .= n, "age" .= a]
+
+instance FromYAML Person where
+   parseYAML = withMap "Person" $ \m -> Person
+       <$> m .: "name"
+       <*> m .: "age"
+
+instance ToYAML Label where
+    -- this generates a Node
+    toYAML (Label n) = mapping [ "Label" .= n]
+
+instance FromYAML Label where
+   parseYAML = withMap "Label" $ \m -> Label
+       <$> m .: "Label"
+
+
+
+  
+test = BLU.fromString "paramsNetwork:\n - apa: 13\n"
+
+stripPos :: [Doc (Node a)] -> [Doc (Node ())]
+stripPos xs = P.map (fmap f) xs
+  where
+    f :: (Node a) -> (Node ())
+    f (Scalar   _ s) = Scalar () s
+    f (Mapping  _ t (mapping)) = Mapping () t (Map.mapKeys f (Map.map f mapping))
+    f (Sequence _ t ns) = Sequence () t (P.map f ns)
+    f (Anchor   _ id n) = Anchor () id (f n)
