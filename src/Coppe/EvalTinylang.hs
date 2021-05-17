ec4ac60d8eca3be46212d8120706be9867f67949
Joel Svensson <svenssonjoel@yahoo.se>
2021-05-17 15:30:57 +0200

    eval tinyLang in progress

--- src/Coppe/EvalTinylang.hs
+++ src/Coppe/EvalTinylang.hs
@@ -57,5 +57,76 @@ evalTiny (EVar i) _ =
        Nothing -> return $
                   Left $
                   EvalError $ "Ident " ++ identToString i ++ "is not present in environment, annotations or hyperparameters."
-     
+evalTiny (EAdd e1 op e2) _ = evalAdd op e1 e2
+evalTiny (EMul e1 op e2) _ = evalMul op e1 e2
+evalTiny (ERel e1 op e2) _ = evalRel op e1 e2
+evalTiny e@(ELam _ _) args = evalLam e args
+
+addAllBindings :: [Exp] -> Value -> Eval (Either EvalError ())
+addAllBindings [] (ListVal []) = return $ Right ()
+addAllBindings (x:xs) (ListVal (v:vs)) =
+  case x of
+    (EVar (Ident s)) -> do addBinding s v
+                           return $ Right ()
+    _ -> return $ Left $ EvalError "Left hand side is not an identifier in binding"                       
+                        
+addAllBindings _ _  = return $ Left $ EvalError "Function application error"
+
  
+
+evalLam :: Exp -> Value -> Eval (Either EvalError Value)
+evalLam = undefined  
+
+  
+noArgs = ListVal []
+
+evalAdd :: AddOp -> Exp -> Exp -> Eval (Either EvalError Value)
+evalAdd aop e1 e2 =
+  do v1 <- evalTiny e1 noArgs
+     v2 <- evalTiny e2 noArgs
+     let op :: Num a => a -> a -> a -- Make Haskell happy.. So needy!
+         op = case aop of
+                Plus -> (+)
+                Minus -> (-)
+     let r = case (v1,v2) of
+               (Right (IntVal i1), Right (IntVal i2)) -> Right $ IntVal (op i1 i2)
+               (Right (IntVal i1), Right (FloatVal f2)) -> Right $ FloatVal (op (fromIntegral i1) f2)
+               (Right (FloatVal f1), Right (IntVal i2)) -> Right $ FloatVal (op f1 (fromIntegral i2))
+               (_, _) -> Left $ EvalError "Arithmetic error"
+     return r
+
+evalMul :: MulOp -> Exp -> Exp -> Eval (Either EvalError Value)
+evalMul mop e1 e2 =
+  do v1 <- evalTiny e1 noArgs
+     v2 <- evalTiny e2 noArgs
+     let r = case mop of
+               Times -> case (v1,v2) of
+                          (Right (IntVal i1), Right (IntVal i2)) -> Right $ IntVal (i1 * i2)
+                          (Right (IntVal i1), Right (FloatVal f2)) -> Right $ FloatVal ((fromIntegral i1) * f2)
+                          (Right (FloatVal f1), Right (IntVal i2)) -> Right $ FloatVal (f1 * (fromIntegral i2))
+                          (_, _) -> Left $ EvalError "Arithmetic error"
+               Div -> case (v1,v2) of
+                        (Right (IntVal i1), Right (IntVal i2)) -> Right $ IntVal (div i1 i2)
+                        (Right (IntVal i1), Right (FloatVal f2)) -> Right $ FloatVal ((fromIntegral i1) / f2)
+                        (Right (FloatVal f1), Right (IntVal i2)) -> Right $ FloatVal (f1 / (fromIntegral i2))
+                        (_, _) -> Left $ EvalError "Arithmetic error"
+     return r
+
+evalRel :: RelOp -> Exp -> Exp -> Eval (Either EvalError Value)
+evalRel rop e1 e2 =
+  do v1 <- evalTiny e1 noArgs
+     v2 <- evalTiny e2 noArgs
+     let op :: (Eq a, Ord a) => a -> a -> Bool
+         op = case rop of
+                LTC -> (<)
+                LEC -> (<=)
+                GTC -> (>)
+                GEC -> (>=)
+                EQC -> (==)
+     let r = case (v1,v2) of
+               (Right (IntVal i1), Right (IntVal i2)) -> Right $ BoolVal (op i1 i2)
+               (Right (IntVal i1), Right (FloatVal f2)) -> Right $ BoolVal (op (fromIntegral i1) f2)
+               (Right (FloatVal f1), Right (IntVal i2)) -> Right $ BoolVal (op f1 (fromIntegral i2))
+               (_, _) -> Left $ EvalError "Arithmetic error"
+     return r
+            
