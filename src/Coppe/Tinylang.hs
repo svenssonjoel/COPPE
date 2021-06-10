f38c2f0d0d25777dceabb4867322bac520f350ab
Joel Svensson <svenssonjoel@yahoo.se>
2021-06-10 06:50:38 +0200

    added missing file to repo

---
+++ src/Coppe/Tinylang.hs
@@ -0,0 +1,16 @@
+module Coppe.Tinylang
+  ( module Coppe.Tinylang.EvalTinylang 
+  , module Coppe.Tinylang.ErrM
+  , module Coppe.Tinylang.AbsTinylang
+  , module Coppe.Tinylang.LexTinylang
+  , module Coppe.Tinylang.ParTinylang
+  , module Coppe.Tinylang.PrintTinylang
+  ) where
+
+import Coppe.Tinylang.EvalTinylang
+import Coppe.Tinylang.ErrM
+import Coppe.Tinylang.AbsTinylang
+import Coppe.Tinylang.LexTinylang
+import Coppe.Tinylang.ParTinylang
+import Coppe.Tinylang.PrintTinylang
+
